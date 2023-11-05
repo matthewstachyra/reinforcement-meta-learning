loss, params, and gradients added to state; removed loss penalty; fixed pool update bugs

src/bounded-discrete/bounded-networks-continuous.py
@@ -1,4 +1,4 @@
-import argparse
+import os
 import math
 import random
 import datetime
@@ -10,7 +10,6 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.nn import Module
-from torch.utils.tensorboard import SummaryWriter
 import gymnasium
 from gymnasium import Env
 from gymnasium.spaces.box import Box
@@ -23,10 +22,13 @@ from typing import (
     Tuple,
     Callable,
 )
+import matplotlib.pyplot as plt
 import stable_baselines3
 from sb3_contrib import RecurrentPPO
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
+from torch.utils.tensorboard import SummaryWriter
+import wandb
 
 # configuration
 default_config = {
@@ -60,11 +62,14 @@ parser.add_argument('--timesteps', '-t', type=int, default=default_config['times
 parser.add_argument('--sb3_model', '-m', type=str, default=default_config['sb3_model'], help='SB3 model to use', required=False)
 parser.add_argument('--sb3_policy', '-p', type=str, default=default_config['sb3_policy'], help='SB3 policy to use', required=False)
 parser.add_argument('--log_dir', '-o', type=str, default=default_config['log_dir'], help='Directory to save tensorboard logs', required=False)
-
 parser.add_argument('--n_tasks', type=int, default=default_config['n_tasks'], help='Number of tasks to generate', required=False)
 parser.add_argument('--n_layers_per_network', type=int, default=default_config['n_layers_per_network'], help='Number of layers per network', required=False)
 args = parser.parse_args()
 config = { key : getattr(args, key, default_value) for key, default_value in default_config.items() }
+wandb.init(
+    project='reinforcement-meta-learning',
+    config=config
+)
 print(f'[INFO] Config={config}')
 
 # create tasks
@@ -141,43 +146,39 @@ for i, (x, y) in enumerate(data):
 
 print(f'[INFO] Layers pre-initialized on tasks.')
 
-class Layer:
-    def __init__(self, 
-                params: Type[torch.nn.Linear]=config['pool_layer_type']):
-        self.params = params
-        self.used = False
-        self.times_used = 0
-
 class LayerPool:
     # pool of uniform Layer objects each with the same type and shape
     def __init__(self, 
                 size: int=config['pool_n_layers'], 
-                layer_type: Type[torch.nn.Linear]=config['pool_layer_type'],
+                layer_constructor: Type[torch.nn.Linear]=config['pool_layer_type'],
                 in_features: int=config['in_features'],
                 out_features: int=config['out_features'],
                 num_nodes_per_layer: int=config['n_nodes_per_layer'],
                 layers: List[torch.nn.Linear]=None):
         self.size = size if layers is None else len(layers)
-        self.layer_type = layer_type
+        self.layer_constructor = layer_constructor
         self.in_features = in_features
         self.out_features = out_features
         self.num_nodes_per_layer = num_nodes_per_layer
 
         # if no layers are passed, create and initialize layer pool from scratch
         if layers is None:
-            self.layers = [Layer(params=self.layer_type(in_features=num_nodes_per_layer, out_features=num_nodes_per_layer))
+            self.layers = [self.layer_constructor(in_features=num_nodes_per_layer, out_features=num_nodes_per_layer)
                 for _ in range(size)]
+            # add initial layers
             for i in range(config['n_tasks']):
-                self.layers[size + i] = self.layer_type(in_features=in_features, out_features=num_nodes_per_layer)
+                self.layers.append(self.layer_constructor(in_features=in_features, out_features=num_nodes_per_layer))
+            # add final layers
             for i in range(config['n_tasks']):
-                self.layers[size + i] = self.layer_type(in_features=num_nodes_per_layer, out_features=out_features)
-            [torch.nn.init.xavier_uniform_(layer.params.weight) for layer in self.layers.values()]
+                self.layers.append(self.layer_constructor(in_features=num_nodes_per_layer, out_features=out_features))
+            [torch.nn.init.xavier_uniform_(layer.weight) for layer in self.layers]
         else:
-            self.layers = [Layer(params=layer) for layer in layers]
+            self.layers = layers
             config['pool_n_layers'] = len(self.layers)
         
     def __str__(self):
         return f"LayerPool(size={self.size}, layer_type={config['pool_layer_type']}, num_nodes_per_layer={config['n_nodes_per_layer']}"
+
 
 class InnerNetworkAction(Enum):
     TRAIN = 0
@@ -221,7 +222,7 @@ class InnerNetwork(gymnasium.Env, Module):
                 action_cache_size: float=config['action_cache_size'],
                 num_workers: int=config['num_workers'],
                 shuffle: bool=True,
-                log_dir: str='runs',
+                log_dir: str=config['log_dir'],
                 ):
         super(InnerNetwork, self).__init__()
         self.epoch = epoch
@@ -239,20 +240,19 @@ class InnerNetwork(gymnasium.Env, Module):
         self.curr = defaultdict(lambda: None)
         self.data_loader = DataLoader(task, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
         self.data_iter = iter(self.data_loader)
-        self.initial_layer = random.choice([layer for layer in self.layer_pool.layers if getattr(layer.params, 'in_features', None) == in_features])
-        self.final_layer = random.choice([layer for layer in self.layer_pool.layers if getattr(layer.params, 'out_features', None) == out_features])
-        self.layers = torch.nn.ModuleList([self.initial_layer.params, self.final_layer.params]) 
-        self.layer_indices = [[layer.params for layer in self.layer_pool.layers].index(layer) for layer in self.layers] 
+        self.initial_layer = random.choice([layer for layer in self.layer_pool.layers if getattr(layer, 'in_features', None) == in_features])
+        self.final_layer = random.choice([layer for layer in self.layer_pool.layers if getattr(layer, 'out_features', None) == out_features])
+        self.layers = torch.nn.ModuleList([self.initial_layer, self.final_layer]) 
+        self.layer_indices = [[layer for layer in self.layer_pool.layers].index(layer) for layer in self.layers] 
         self.loss_fn = torch.nn.MSELoss()
         self.opt = torch.optim.Adam(self.layers.parameters(), lr=self.learning_rate)
         self.actions_taken = [InnerNetworkAction.TRAIN] * config['action_cache_size']
         self.writer = SummaryWriter(log_dir=log_dir)
         self.state = self.reset()
         state_shape = self.build_state().shape
-        self.observation_space = gymnasium.spaces.box.Box(low=float('-inf'), high=float('inf'), shape=state_shape) # TODO is to normalize
+        self.observation_space = gymnasium.spaces.box.Box(low=float('-inf'), high=float('inf'), shape=state_shape)
         self.action_space = gymnasium.spaces.discrete.Discrete(self.layer_pool.size * 3)
         self.timestep = 0
-
 
     def step(self, action: np.int64) -> Tuple[torch.Tensor, float, bool, dict]: 
         assert action.shape == (), f'[ERROR] Expected action shape () for scalar {self.action_space.n}, got: {action.shape}'
@@ -266,7 +266,7 @@ class InnerNetwork(gymnasium.Env, Module):
         self.run_inner_network()
         s_prime = self.build_state()
         reward = self.reward()
-        self.tensorboard_log()
+        self.log()
 
         return (
             s_prime,
@@ -287,15 +287,17 @@ class InnerNetwork(gymnasium.Env, Module):
         if (add_action_type):
             if (len(self.layers)==config['n_layers_per_network']):
                 self.curr['action_type'] = InnerNetworkAction.ERROR
-            elif (self.layer_pool.layers[action] in self.layers):
+            # elif (self.layer_pool.layers[action] in self.layers):
+            #     self.curr['action_type'] = InnerNetworkAction.ERROR
+            elif self.layer_pool.layers[action].in_features==self.in_features or self.layer_pool.layers[action].out_features==self.out_features:
                 self.curr['action_type'] = InnerNetworkAction.ERROR
             else:
                 self.curr['action_type'] = InnerNetworkAction.ADDTRAIN
         elif (delete_action_type):
             action = action - self.layer_pool.size
-            if (self.layer_pool.layers[action].params not in self.layers):
+            if (self.layer_pool.layers[action] not in self.layers):
                 self.curr['action_type'] = InnerNetworkAction.ERROR
-            elif self.layer_pool.layers[action].params.in_features==self.in_features or self.layer_pool.layers[action].params.out_features==self.out_features:
+            elif self.layer_pool.layers[action].in_features==self.in_features or self.layer_pool.layers[action].out_features==self.out_features:
                 self.curr['action_type'] = InnerNetworkAction.ERROR
             else:
                 self.curr['action_type'] = InnerNetworkAction.DELETETRAIN
@@ -307,28 +309,27 @@ class InnerNetwork(gymnasium.Env, Module):
     def update_inner_network(self, action: np.int64) -> None:
         index = self.decide_action_type(action)
         if (self.curr['action_type']==InnerNetworkAction.ADDTRAIN): 
-            new_layer = self.layer_pool.layers[index].params
-            # new initial layer
-            if new_layer.in_features==self.in_features:
-                self.layers.pop(0) 
-                self.layers.insert(0, new_layer)
-            # new final layer
-            elif new_layer.out_features==self.out_features:
-                self.layers.pop(-1)
-                self.layers.append(new_layer)
-            else:
-                final_layer = self.layers.pop(-1) 
-                self.layers.append(new_layer)
-                self.layers.append(final_layer) 
+            new_layer = self.layer_pool.layers[index]
+            # # new initial layer
+            # if new_layer.in_features==self.in_features:
+            #     self.layers.pop(0) 
+            #     self.layers.insert(0, new_layer)
+            # # new final layer
+            # elif new_layer.out_features==self.out_features:
+            #     self.layers.pop(-1)
+            #     self.layers.append(new_layer)
+            final_layer = self.layers.pop(-1) 
+            self.layers.append(new_layer)
+            self.layers.append(final_layer) 
         elif (self.curr['action_type']==InnerNetworkAction.DELETETRAIN):
             layers_copy = self.layers
-            self.layers = torch.nn.ModuleList([layer for layer in self.layers if layer!=self.layer_pool.layers[index].params])
+            self.layers = torch.nn.ModuleList([layer for layer in self.layers if layer!=self.layer_pool.layers[index]])
             assert len(layers_copy)!=len(self.layers), '[ERROR] Delete on inner network failed.'
         elif (self.curr['action_type']==InnerNetworkAction.TRAIN or self.curr['action_type']==InnerNetworkAction.ERROR):
             return 
         else: 
             raise Exception(f"[ERROR] Unexpected action returned by inner network: {self.curr['action_type']}")
-        self.layer_indices = [[layer.params for layer in self.layer_pool.layers].index(layer) for layer in self.layers] 
+        self.layer_indices = [[layer for layer in self.layer_pool.layers].index(layer) for layer in self.layers] 
             
     def next_batch(self, throw_exception=False) -> None:
         self.prev = self.curr
@@ -373,7 +374,7 @@ class InnerNetwork(gymnasium.Env, Module):
         assert self.curr['y_hat'].dtype == torch.float32
 
     def build_state(self) -> np.ndarray:
-        one_hot_network_layers = torch.tensor(np.array([1 if self.layer_pool.layers[i].params in self.layers else 0 
+        one_hot_network_layers = torch.tensor(np.array([1 if self.layer_pool.layers[i] in self.layers else 0 
                                    for i in range(len(self.layer_pool.layers))]))
         num_add_actions = torch.tensor(len(list(filter(lambda e : e == InnerNetworkAction.ADDTRAIN, self.actions_taken)))).unsqueeze(0)
         num_delete_actions = torch.tensor(len(list(filter(lambda e : e == InnerNetworkAction.DELETETRAIN, self.actions_taken)))).unsqueeze(0)
@@ -408,20 +409,24 @@ class InnerNetwork(gymnasium.Env, Module):
         elif (self.curr['action_type'] == InnerNetworkAction.DELETETRAIN or self.curr['action_type'] == InnerNetworkAction.TRAIN):
             reward = loss_delta
         elif (self.curr['action_type'] == InnerNetworkAction.ERROR):
-            reward = -50
+            reward = -1
         else:
             raise Exception('[ERROR] Invalid action type.')
         self.curr['reward'] = reward
         return reward
 
-    def tensorboard_log(self):
+    def log(self):
         task_num = str(self.curr['info']['i'].item())
         self.writer.add_scalar(f'loss_epoch{self.epoch}_task{task_num}', self.curr['loss'], global_step=self.timestep) 
-        self.writer.add_scalar(f'num_layers_epoch{self.epoch}_task{task_num}', len(self.layers), global_step=self.timestep) 
+        wandb.log({ f'loss_epoch{self.epoch}_task{task_num}' : self.curr['loss'] })
+        # self.writer.add_scalar(f'num_layers_epoch{self.epoch}_task{task_num}', len(self.layers), global_step=self.timestep) 
         self.writer.add_scalar(f'reward_epoch{self.epoch}_task{task_num}', self.curr['reward'], global_step=self.timestep)
+        wandb.log({ f'reward_epoch{self.epoch}_task{task_num}' : self.curr['reward'], })
         if (len(self.layers)!=0):
             self.writer.add_histogram(f'pool_indices_epoch{self.epoch}_task{task_num}', torch.tensor(self.layer_indices).long(), global_step=self.timestep) 
+            wandb.log({ f'pool_indices_epoch{self.epoch}_task{task_num}' : wandb.Histogram(torch.tensor(self.layer_indices))})
         self.writer.add_histogram(f'action_types_epoch{self.epoch}_task{task_num}', torch.tensor([e.value for e in self.actions_taken]).long(), global_step=self.timestep) 
+        wandb.log({ f'action_types_epoch{self.epoch}_task{task_num}' : wandb.Histogram(torch.tensor([e.value for e in self.actions_taken]))})
 
     def reset(self, seed=None) -> np.ndarray:
         self.eval()
@@ -463,9 +468,9 @@ class REML:
         # we use the same policy, swapping out envs for the n tasks, m times. 
         # therefore the number of steps is (timesteps)*(n)*(m)
         for epoch in range(self.epochs):
-            print(f'[INFO] Epoch {epoch + 1}/{self.epochs}')
+            print(f'[INFO] Epoch={epoch + 1}/{self.epochs}')
             for i, task in enumerate(self.tasks): 
-                print(f'[INFO] Task num={i+1}/{len(self.tasks)}')
+                print(f'[INFO] Task={i+1}/{len(self.tasks)}')
 
                 # each task gets its own network
                 self.env = gymnasium.wrappers.NormalizeObservation(InnerNetwork(epoch, task, self.layer_pool, log_dir=self.log_dir))
@@ -476,48 +481,40 @@ class REML:
                 model.learn(total_timesteps=self.timesteps, tb_log_name=f'epoch_{epoch}_task_{i}', reset_num_timesteps=True)
 
                 # log results
-                self.env.writer.add_scalar(f'loss/epoch{epoch}_task{i}', self.env.curr['loss'], global_step=epoch) 
+                # TODO is to uncomment loss values to observe effect over epochs
+                # self.env.writer.add_scalar(f'loss/epoch{epoch}_task{i}', self.env.curr['loss'], global_step=epoch) 
+                # wandb.log({ f'loss/epoch{epoch}_task{i}' : self.env.curr['loss'] })
                 yhats = self.evaluate_inner_network()
                 for x, yhat in zip(self.env.task.data, yhats):
                     self.env.writer.add_scalar(f'sin_curve/epoch{epoch}_task{i}', yhat, global_step=x)
+                plot_name = f'sine_curve_epoch_{epoch}_task_{i}'
+                plot_path = f'{self.log_dir}/{plot_name}.png'  
+                plt.figure()
+                plt.plot(self.env.task.data, [yhat.detach().numpy() for yhat in yhats])
+                plt.plot(self.env.task.data, self.env.task.targets)
+                plt.title(plot_name)
+                plt.savefig(plot_path)
+                wandb.log({plot_name: wandb.Image(plot_path)})
                 self.env.writer.close()
 
-		        # save new pool
-                for i, updated_layer_params in enumerate(self.env.layers):
+                # save new pool
+                for i, updated_layer in enumerate(self.env.layers):
                     pool_index = self.env.layer_indices[i]
-                    assert updated_layer_params.in_features==self.layer_pool.layers[pool_index].params.in_features, '[ERROR]'
-                    assert updated_layer_params.out_features==self.layer_pool.layers[pool_index].params.out_features, '[ERROR]'
-                    assert updated_layer_params!=self.layer_pool.layers[pool_index], '[ERROR]'
-                    self.layer_pool.layers[pool_index].params = updated_layer_params
-                    self.layer_pool.layers[pool_index].used = True
-                    self.layer_pool.layers[pool_index].times_used += 1
+                    assert updated_layer.in_features==self.layer_pool.layers[pool_index].in_features, '[ERROR]'
+                    assert updated_layer.out_features==self.layer_pool.layers[pool_index].out_features, '[ERROR]'
+                    assert updated_layer!=self.layer_pool.layers[pool_index], '[ERROR]'
+                    self.layer_pool.layers[pool_index] = updated_layer
                 print(f'[INFO] Updated pool layers.')
     
-    def evaluate_inner_network(self):
-        try:
-            y_hats = []
-            self.env.eval()
-            # new dataloader to go through full dataset
-            self.env.data_loader = DataLoader(self.env.task, batch_size=self.env.batch_size, shuffle=self.env.shuffle, num_workers=self.env.num_workers)
-            self.env.data_iter = iter(self.env.data_loader)
-            while True:
-                self.env.next_batch(throw_exception=True)
-                x = self.env.curr['x']
-                for i in range(len(self.env.layers) - 1): 
-                    x = torch.nn.functional.relu(self.env.layers[i](x))
-                    # print(f'interim x: {x}') # expecting it be 0 value
-                self.env.curr['latent_space'] = x
-                # print(f'bias: {self.env.layers[-1].bias}')
-                self.env.curr['y_hat'] = self.env.layers[-1](x) 
-                self.env.curr['prev_loss'] = self.env.curr['loss']
-                self.env.curr['loss'] = self.env.loss_fn(self.env.curr['y'], self.env.curr['y_hat'])
-
-                # print(f"[INFO] x={self.env.curr['x']}, y_hat={self.env.curr['y_hat']}, y={self.env.curr['y']}")
-                y_hats.append(self.env.curr['y_hat'])
-        except StopIteration:
-            pass
-        finally:
-            return y_hats
+    def evaluate_inner_network(self) -> List[torch.Tensor]:
+        self.env.eval()
+        y_hats = []
+        for x in X:
+            x = torch.Tensor([x])
+            for i in range(len(self.env.layers) - 1): 
+                x = torch.nn.functional.relu(self.env.layers[i](x))
+            y_hats.append(self.env.layers[-1](x))
+        return y_hats
 
 if __name__ == "__main__":
     tasks = [InnerNetworkTask(data=tasks_data[i], targets=tasks_targets[i], info=tasks_info[i]) for i in range(config['n_tasks'])]